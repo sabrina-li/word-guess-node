ba23a5ade818303dc8e82194fc9003b9e5bcf6f5
Sabrina L <sabrinali0312@gmail.com>
2019-04-16 22:55:34 -0400

    add inquirer

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const 
+const  Word = require("./word");
+const inquirer = require('inquirer');
+
+
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
